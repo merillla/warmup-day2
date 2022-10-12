fcb66bb6f989bc1b0717cabd61e6b3c1805d1c04
merillla <merillalaitiomaki@icloud.com>
2022-10-12 11:11:10 +0200

    initial commit for setting up the project

---
+++ WarmUppTest.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+
+
+
+
+
+
+
+
+
+public class WarmUppTest {
+    public static void main(String[] args) {
+        String[] dogs = {"Poodle", "Chihu", "Bulldog"};
+        int []
+        System.out.println(dogs[2]);
+
+    }
+}
